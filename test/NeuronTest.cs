0fde904b73b98640cb298c0bf56b2b01eb79d577
TheRusskiy <dimanrussian@gmail.com>
2013-03-09 04:05:39 +0400

    Initial commit

---
+++ test/NeuronTest.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NeuralNetwork.src;
+
+namespace NeuralNetwork.test
+{   
+    class NeuronTest
+    {
+        private Neuron neuron;
+        private InputNeuron input;
+        [SetUp]
+        public void SetUp()
+        {
+            neuron =  new Neuron();
+            input = new InputNeuron();
+        }
+        [Test]
+        public void ActivateInput()
+        {
+            Assert.Throws(typeof (NotConfiguredException),
+                () => input.Activation());
+            input.Input = 1;
+            Assert.AreEqual(input.Activation(), 1);
+        }
+        [Test]
+        public void ActivateNeuron()
+        {
+            Assert.Throws(typeof(NotConfiguredException),
+                          () => neuron.Activation());
+            InputNeuron i1 = new InputNeuron();
+            InputNeuron i2 = new InputNeuron();
+            InputNeuron i3 = new InputNeuron();
+            i1.Input = 1;
+            i2.Input = 1;
+            i3.Input = 1;
+            double w1 = 1;
+            double w2 = 1;
+            double w3 = 1;
+            neuron.Connect(i1, w1);
+            neuron.Connect(i2, w2);
+            neuron.Connect(i3, w3);
+            double tx = i1.Input*w1+i2.Input*w2+i3.Input*w3;
+            double expected_activation = 1/(1 + Math.Pow(Math.E, -tx));
+            AssertClose(neuron.Activation(), expected_activation);
+        }
+        [Test]
+        public void ConnectToNeuron()
+        {
+            InputNeuron input1 = new InputNeuron();
+            InputNeuron input2 = new InputNeuron();
+            InputNeuron input3 = new InputNeuron();
+            neuron.Connect(input1, 0);
+            neuron.Connect(input2, 0);
+            neuron.Connect(input3, 0);
+            INeuron[] neurons = neuron.Neurons;
+            Assert.Contains(input1, neurons);
+            Assert.Contains(input2, neurons);
+        }
+        [Test]
+        public void WeightsChange()
+        {
+            neuron.Connect(new InputNeuron(), 0);
+            Assert.AreEqual(neuron.Weights[1], 0);
+            neuron.SetWeight(1, 2.0);
+            Assert.AreEqual(neuron.Weights[1], 2);
+        }
+
+        [Test]
+        public void TestLength()
+        {
+            Assert.AreEqual(neuron.Length, 1);
+            neuron.Connect(new Neuron());
+            Assert.AreEqual(neuron.Length, 2);
+        }
+
+        [Test]
+        public void TestOnlySingleBias()
+        {
+            Assert.Throws(typeof(MoreThanOneBiasException),
+                          () => neuron.Connect(new BiasNeuron()));
+        }
+        [Test]
+        public void TestInputNeuron()
+        {
+            input.Input=1;
+            Assert.AreEqual(input.Activation(), 1);
+        }
+        public static void AssertClose(double arg1, double arg2, double by=0.0001)
+        {
+            Assert.Less(Math.Abs(arg1 - arg2), by);
+        }
+    }
+    
+}
